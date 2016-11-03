e8370725130748402bd24f9b6a16fd19e0c5d199
derek_robertson <dwilliamrobertson@gmail.com>
2016-11-03 15:32:55 -0700

    added dynamic weather icons

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 //api key = 7c1036ddfe7bb328dfb9f941f670c99e;
+//http://api.openweathermap.org/img/w/{code}.png
 
 angular.module('weatherApp', [])
 
 .run(['$rootScope', '$http', function($rootScope, $http){
+  // var list  = {
+  //   "clear-day", //clear sky
+  //   "partly-cloudy-day", // broken clouds, few clouds 
+  //   "cloudy", //scattered clouds
+  //   "rain", //rain, shower rain, thunderstorm
+  //   "snow", //snow
+  //   "fog"  //mist
+  // }
+
+
   function getLocation(){
     if(navigator.geolocation){
       // console.log(navigator.geolocation.getCurrentPositon());
@@ -16,13 +27,19 @@ angular.module('weatherApp', [])
           method: 'GET',
           params: {
             lat: lat,
-            lon: lng
+            lon: lng,
+            units: 'imperial'
           }
         }
 
         $http(req).then(function success(res){
           console.log('success: ', res);
           $rootScope.res = res.data;
+          $rootScope.icon = res.data.weather[0].description;
+          console.log("Icon:", $rootScope.icon)
+
+          setIcon();
+
         }, function error(res){
           console.log('error: ', res);
         })
@@ -30,15 +47,59 @@ angular.module('weatherApp', [])
     } else {
       alert('No Browser Supported geolocation');
     }
+
+    //-------------------------------------------------
+    function setIcon(){
+
+      var icons = new Skycons();
+
+      var giph;
+
+      switch($rootScope.icon) {
+        case "mist": 
+          giph = "fog"
+          break;
+        case "broken clouds":
+          giph = "partly-cloudy-day"
+          break;
+        case "few clouds": 
+          giph = "partly-cloudy-day"
+          break;
+        case "clear sky":
+          giph = "clear-day"
+          break;
+        case "scattered clouds":
+          giph = "cloudy"
+          break;
+        case "rain":
+          giph = "rain"
+          break;
+        case "shower rain":
+          giph = "rain"
+          break;
+        case "thunderstorm":
+          giph = "rain"
+          break;
+        case "snow":
+          giph = "snow"
+          break;
+      }
+     
+      icons.set("icon1", giph);
+
+      icons.play();
+
+    }
+    
   }
 
   getLocation();
 }])
 
 .controller('weatherCtrl', ['$scope', '$http', function($scope, $http){
-  console.log($scope.res);
 
-  $scope.showInfo = function() {
-    console.log($scope.res);
+  $scope.toggleDegrees = function(){
+
   }
+
 }]);
